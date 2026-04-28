5a15013e70ba707348ed6e05bc43b23e066fa75a
Jacob Arlo Needham <jacob.arlo.needham@gmail.com>
2026-04-27 21:18:43 -0400

    preped mobile scaling

--- sw.js
+++ sw.js
@@ -1,7 +1,26 @@
+const CACHE_NAME = "gmplayer-v1"
+
+const ASSETS = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/GoblinAE.js"
+];
+
+
 self.addEventListener("install", event => {
-    console.log("Service Worker Installed");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        })
+    );
+    
 });
 
 self.addEventListener("fetch", event => {
-    //for now: just pass requests through
+    event.resondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
